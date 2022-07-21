8e4b8176c34792eca7216cb886eafc2e4437d12f
Vonckad <Vonkad@yandex.ru>
2022-07-21 23:35:36 +0300

    Initial Commit

---
+++ CorpusSecondTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CorpusSecondTask
+//
+//  Created by Vlad Ralovich on 21.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
