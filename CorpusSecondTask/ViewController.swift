108669f9b2f905b4ebb458b46678b4ad5e0ced9d
Vonckad <Vonkad@yandex.ru>
2022-07-21 23:44:05 +0300

    delete storyboard

--- CorpusSecondTask/ViewController.swift
+++ CorpusSecondTask/ViewController.swift
@@ -11,9 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Main VC"
+        view.backgroundColor = .brown
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
-
-
 }
 
